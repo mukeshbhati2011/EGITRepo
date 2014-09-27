b1a15a420b4d1e0aed8ee0409c312df8f5e07320
mukeshbhati2011 <bhati.mukesh@gmail.com>
2014-09-27 17:10:26 -0400

    Update HelloWorld.java
    Changes done in Github directly

--- EGITJavaProject/src/HelloWorld.java
+++ EGITJavaProject/src/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld
 	{
 		System.out.println("Jai Mata Di");
 		System.out.println(" Test Code Addition code in hello World");
+		System.out.println("Changes done in Github");
 	}
 }
