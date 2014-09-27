814ee1016c887bff72bbcd63305ef5333b233abf
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-27 14:32:01 -0400

    Commit changes in Master

--- EGITJavaProject/src/TestClass.java
+++ EGITJavaProject/src/TestClass.java
@@ -4,8 +4,9 @@ public class TestClass {
 	public static void main(String[] args)
 	{
 
-		System.out.println("Added Child branhc changes - This is imp - Add to index");
-		System.out.println("Child 1 stuff added");
+		System.out.println("Added Child branhc changes - This is imp - Add to index -  Child 2 ");
+		System.out.println("Child 2 ");
+		System.out.println("Child 2 - New row");
 
 	}
 
