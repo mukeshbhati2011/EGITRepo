9f90ddb39555aee65566c6e38187758e1088a0c2
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-27 17:48:47 -0400

    Commit after merging stuff in Child Repo

--- EGITJavaProject/src/HelloWorld.java
+++ EGITJavaProject/src/HelloWorld.java
@@ -10,5 +10,7 @@ public class HelloWorld
 	{
 		System.out.println("Jai Mata Di");
 		System.out.println(" Test Code Addition code in hello World");
+		System.out.println("Changes done in Github - change added");
+		System.out.println("Fourth Line added");
 	}
 }
---
+++ EGITJavaProject/src/HelloWorld6.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld6 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("New class HelloWordl 6");
+	}
+}
