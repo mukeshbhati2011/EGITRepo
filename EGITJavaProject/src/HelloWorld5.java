cec586a9a3343e198ff931baa8c3d92fdef62439
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-27 18:03:26 -0400

    Comiting TestChild Changes

--- EGITJavaProject/src/HelloWorld5.java
+++ EGITJavaProject/src/HelloWorld5.java
@@ -4,6 +4,7 @@ public class HelloWorld5 {
 	public static void main(String[] args)
 	{
 		System.out.println("Test Hello World 5");
+		System.out.println("Test Hello World 5 - New Line");
 	}
 
 }
--- EGITJavaProject/src/TestClass.java
+++ EGITJavaProject/src/TestClass.java
@@ -1,10 +1,14 @@
 
-public class TestClass 
-{
+public class TestClass {
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Added Child branhc changes - This is imp - Add to index");
-		System.out.println("Child 1 stuff added");
+
+		System.out.println("Added Child branhc changes - This is imp - Add to index -  Child 2 ");
+		System.out.println("Child 2 ");
+		System.out.println("Child 2 - New row");
+		System.out.println("Addded in Master");
+
 	}
 
 }
