a3a52ca571c5245ec80f505f0e0e3a40b85b389a
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-27 14:05:57 -0400

    Committing child branch changes

--- EGITJavaProject/src/TestClass.java
+++ EGITJavaProject/src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Added Child branhc changes");
+		System.out.println("Added Child branhc changes - This is imp");
 	}
 
 }
