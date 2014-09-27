d67888984e6b17c0825f63e6cff7f906ebb97a83
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-27 14:14:23 -0400

    Child 2 stuff added

--- EGITJavaProject/src/TestClass.java
+++ EGITJavaProject/src/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass {
 
 		System.out.println("Added Child branhc changes - This is imp - Add to index -  Child 2 ");
 		System.out.println("Child 2 ");
+		System.out.println("Child 2 - New row");
 
 	}
 
