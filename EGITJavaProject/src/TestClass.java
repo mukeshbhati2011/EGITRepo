eb32e366955566879a6e0c1b7decd82b87c5fd33
Mukesh Bhati <bhati.mukesh@gmail.com>
2014-09-27 14:13:01 -0400

    Child 2 branch changes committed

--- EGITJavaProject/src/TestClass.java
+++ EGITJavaProject/src/TestClass.java
@@ -4,7 +4,8 @@ public class TestClass {
 	public static void main(String[] args)
 	{
 
-		System.out.println("Added Child branhc changes - This is imp - Add to index");
+		System.out.println("Added Child branhc changes - This is imp - Add to index -  Child 2 ");
+		System.out.println("Child 2 ");
 
 	}
 
